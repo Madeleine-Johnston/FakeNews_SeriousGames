22383aac54625f9e662715376ef985926e03eba7
Madeleine Johnston <M.Johnston1@student.gsa.ac.uk>
2019-04-01 16:55:27 +0100

    Initial Commit
    Intro Scene, Scene 1

---
+++ FakeNewsProject/Assets/Scripts/MenuScripts/ObjInteraction.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjInteraction : MonoBehaviour {
+
+    public Collider coll;
+    public GameObject laptopClick;
+    public bool click = false;
+    public GameObject laptopUI;
+
+    // Use this for initialization
+    void Start () {
+        coll = GetComponent<Collider>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0) && click == false)
+        {
+            //send raycast to detect collision
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            //if collision by raycast and object
+            if (coll.Raycast(ray, out hit, 100.0f))
+            {
+                //displays info 
+                laptopClick.SetActive(false);
+                laptopUI.SetActive(true);
+                click = true;
+            }
+        }
+
+        else
+        {
+            if (Input.GetMouseButtonDown(0) && click == true)
+            {
+                //removes info text on screen
+                Debug.Log("set false");
+
+                //send raycast to detect collision
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                //if collision by raycast and object
+                if (coll.Raycast(ray, out hit, 100.0f))
+                {
+                    //displays info 
+                    laptopClick.SetActive(false);
+                    laptopUI.SetActive(false);
+                    click = false;
+                }
+            }
+        }
+    }
+
+        
+}
---
+++ FakeNewsProject/Assets/Scripts/MenuScripts/UIControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//This class is used by any of the UI elements to run methods
+public class UIControl : MonoBehaviour 
+{
+	public AudioSource myClip;
+
+	//Quits the app
+	public void ChangeToOutro()
+	{
+		SceneManager.LoadScene("Outro");
+	}
+
+	public void ChangeToGameSceneStarts()
+	{
+		Invoke("ChangeToGameScene", 1.3f);
+	}
+
+	//Changes scene
+	public void ChangeToGameScene()
+	{
+		SceneManager.LoadScene("02");				
+	}
+	public void ChangeToMenuStarts()
+	{
+		Invoke("ChangeToMenu", 1.3f);
+	}
+
+	//Changes scene
+	public void ChangeToMenu()
+	{
+		SceneManager.LoadScene("01");				
+	}
+
+}
